70c8de42f02e6a5a1691085360baa55a50630c1a
crin706 <rosh-ka@mail.ru>
2024-04-26 12:28:50 +0300

    phone class done

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        Phone nokia = new Phone(8999887766L, "Nokia", 213.2, "China");
+        Phone xiaomi = new Phone(8999887777L, "Xiaomi", 203.5, "China");
+        Phone apple = new Phone();
+        System.out.println(nokia);
+        System.out.println(xiaomi);
+        System.out.println(apple);
+        nokia.receiveCall("Allice");
+        System.out.println();
+        xiaomi.receiveCall("Peter", 8999887777L);
     }
 }
